11b69d60154fb3e80585e9d766f1a7cacdbd99e7
ferrighi <laraf@met.no>
2019-06-17 12:08:40 +0200

    Update keyval.py
    adding one more key:val

--- keyval.py
+++ keyval.py
@@ -2,6 +2,7 @@
 keyvals = {'key1': 'value1',
            'key2': 'value2',
            'key3': 'value3',
-           'key4': 'value4'}
+           'key4': 'value4',
+           'key5': 'value5'}
 
 
