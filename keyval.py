45e668b704c47642f548312cfca30594e5a35fe2
laraf <laraf@met.no>
2019-06-17 12:07:04 +0200

    this is a simple change

--- keyval.py
+++ keyval.py
@@ -1,6 +1,7 @@
 # istantiate a simple python dictionary
 keyvals = {'key1': 'value1',
            'key2': 'value2',
-           'key3': 'value3'}
+           'key3': 'value3',
+           'key4': 'value4'}
 
 
